6c358866be26e989b11ee3205c728a48f3905834
Eric Engelhart <bob80333@gmail.com>
2022-05-11 09:21:21 -0400

    pytorch version

--- filter_lowbandwidth.py
+++ filter_lowbandwidth.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 import argparse
 from pathlib import Path
 import soundfile
+import torch
+import torchaudio
 
 
 # implement power spectrogram
@@ -31,12 +33,38 @@ def rolloff_filtered(audio, db=50, t=0.99):
     return np.nanmin(ind * freq, axis=-2, keepdims=True)
 
 
+def spec_torch(audio):
+    S = torchaudio.functional.spectrogram(
+        audio, n_fft=2400, hop_length=600, win_length=2400,
+        window=torch.hann_window(2400).cuda(), center=True, pad_mode="constant", power=2, pad=0, normalized=False
+    )
+    return S
+
+
+def torch_rolloff_filtered(audio, db=50, t=0.99):
+    S = spec_torch(audio)
+    S_log = torch.log(S)
+    max_db = torch.max(S_log)
+    db_filter = max_db - db
+    S_log[S_log < db_filter] = -10000
+    S = torch.exp(S_log)
+    total_energy = torch.cumsum(S, dim=-2)
+    threshold = t * total_energy[..., -1, :]
+    threshold = torch.unsqueeze(threshold, dim=-2)
+    ind = torch.where(total_energy < threshold, torch.tensor(np.nan, dtype=audio.dtype).cuda(), torch.tensor(1, dtype=audio.dtype).cuda())
+    freq = torch.unsqueeze(torch.tensor(librosa.fft_frequencies(48000, 2400)).cuda(), dim=-1)
+    freqs = torch.min(torch.nan_to_num(ind * freq), dim=-2, keepdim=True).values
+    return freqs
+
+
 def max_frequency(audio):
     return np.max(rolloff_filtered(audio))
+
 
 # this will better find max frequency for audio with high frequency noise
 def max_frequency_aggresive(audio):
     return np.max(rolloff_filtered(audio, db=20, t=0.95))
+
 
 # the difference between regular and aggresive will determine false positives from regular
 
