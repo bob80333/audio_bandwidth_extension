1ee873dbbaaeede6096f24c981e016c87410d40f
Eric Engelhart <bob80333@gmail.com>
2022-05-16 11:22:15 -0400

    initial mup implementation

---
+++ mup_audio_unet.py
@@ -0,0 +1,265 @@
+import torch
+import torch.nn as nn
+import mup
+
+
+# from this paper: https://arxiv.org/abs/2011.02421
+# one-shot conditional audio filtering of arbitrary sounds
+# skipping the FiLM layers, as we're just filtering everything but speech out
+
+class ResidualUnit(nn.Module):
+
+    def __init__(self, num_channels, dilation):
+        super().__init__()
+
+        self.layers = nn.Sequential(
+            nn.utils.weight_norm(
+                nn.Conv1d(num_channels, num_channels, kernel_size=(3,), dilation=dilation, padding=dilation,
+                          padding_mode='replicate')),
+            # nn.GroupNorm(16, num_channels),
+            nn.ELU(inplace=True),
+            nn.utils.weight_norm(nn.Conv1d(num_channels, num_channels, kernel_size=(1,))),
+            # nn.GroupNorm(16, num_channels),
+            nn.ELU(inplace=True),
+        )
+
+        self.skip = SkipConnection(num_channels)
+
+    def forward(self, x):
+        return self.layers(x) + self.skip(x)
+
+
+class SkipConnection(nn.Module):
+
+    def __init__(self, num_channels, output_channels=None):
+        super().__init__()
+
+        if output_channels is None:
+            output_channels = num_channels
+
+        layers = [
+            nn.utils.weight_norm(nn.Conv1d(num_channels, output_channels, kernel_size=(1,))),
+        ]
+
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class EncoderBlock(nn.Module):
+
+    def __init__(self, channels, stride, n_res_units=3):
+        super().__init__()
+
+        layers = []
+
+        for i in range(n_res_units):
+            layers.append(ResidualUnit(channels, 3 ** i))
+
+        layers.append(
+            nn.utils.weight_norm(
+                nn.Conv1d(channels, 2 * channels, kernel_size=(2 * stride,), stride=(stride,), padding=stride // 2,
+                          padding_mode='replicate')))
+
+        # layers.append(nn.GroupNorm(16, 2 * channels))
+        layers.append(nn.ELU(inplace=True))
+
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class DecoderBlock(nn.Module):
+
+    def __init__(self, channels, stride, n_res_units=3):
+        super().__init__()
+
+        layers = []
+
+        layers.append(
+            nn.utils.weight_norm(
+                nn.ConvTranspose1d(channels, channels // 2, kernel_size=(2 * stride,), stride=(stride,),
+                                   padding=stride))
+        )
+
+        layers.append(nn.ReplicationPad1d(stride // 2))
+
+        # layers.append(nn.GroupNorm(16, channels // 2))
+        layers.append(nn.ELU(inplace=True))
+
+        for i in range(n_res_units):
+            layers.append(ResidualUnit(channels // 2, 3 ** i))
+
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class MupAudioUNet(nn.Module):
+
+    def __init__(self, base_channels=32, input_channels=1, output_channels=1, n_res_units=3):
+        super().__init__()
+
+        self.input_conv = nn.Sequential(
+            nn.utils.weight_norm(
+                nn.Conv1d(input_channels, base_channels, kernel_size=(7,), padding=3, padding_mode='replicate')),
+            # nn.GroupNorm(16, base_channels),
+            nn.ELU(inplace=True)
+        )
+
+        self.input_skip = SkipConnection(input_channels, output_channels)
+
+        self.encoder_blocks = nn.ModuleList([
+            EncoderBlock(base_channels, stride=2, n_res_units=n_res_units),
+            EncoderBlock(base_channels * 2, stride=2, n_res_units=n_res_units),
+            EncoderBlock(base_channels * 4, stride=8, n_res_units=n_res_units),
+            EncoderBlock(base_channels * 8, stride=8, n_res_units=n_res_units),
+        ])
+
+        self.middle_layers = nn.Sequential(
+            nn.utils.weight_norm(nn.Conv1d(base_channels * 16, base_channels * 16, kernel_size=(7,), padding=3,
+                                           padding_mode='replicate')),
+            # nn.GroupNorm(16, base_channels * 16),
+            nn.ELU(inplace=True),
+            nn.utils.weight_norm(nn.Conv1d(base_channels * 16, base_channels * 16, kernel_size=(7,), padding=3,
+                                           padding_mode='replicate')),
+            # nn.GroupNorm(16, base_channels * 16),
+            nn.ELU(inplace=True),
+        )
+
+        self.middle_skip = SkipConnection(base_channels * 16)
+
+        self.decoder_blocks = nn.ModuleList([
+            DecoderBlock(base_channels * 16, stride=8, n_res_units=n_res_units),
+            DecoderBlock(base_channels * 8, stride=8, n_res_units=n_res_units),
+            DecoderBlock(base_channels * 4, stride=2, n_res_units=n_res_units),
+            DecoderBlock(base_channels * 2, stride=2, n_res_units=n_res_units),
+        ])
+
+        self.decoder_skips = nn.ModuleList([
+            SkipConnection(base_channels * 8),
+            SkipConnection(base_channels * 4),
+            SkipConnection(base_channels * 2),
+            SkipConnection(base_channels),
+        ])
+
+        self.out_conv = nn.Sequential(
+            nn.utils.weight_norm(
+                mup.MuConv1d(in_channels=base_channels, out_channels=output_channels, kernel_size=7, padding=3,
+                             padding_mode='replicate')),
+        )
+
+    def forward(self, input):
+        # skip connections
+        xs = []
+        x = self.input_conv(input)
+
+        for block in self.encoder_blocks:
+            xs.append(x)
+            x = block(x)
+
+        xs.append(x)
+        x = self.middle_layers(x)
+        x = x + self.middle_skip(xs.pop())
+
+        for block, skip in zip(self.decoder_blocks, self.decoder_skips):
+            x = block(x)
+            x = x + skip(xs.pop())
+
+        x = self.out_conv(x)
+
+        return x + self.input_skip(input)
+
+
+# testing purposes, mup coord checks, less layers on the chart
+class VeryShallowMupAudioUNet(nn.Module):
+
+    def __init__(self, base_channels=2, input_channels=1, output_channels=1, n_res_units=1, ismup=True):
+        super().__init__()
+
+        self.input_conv = nn.Sequential(
+            nn.utils.weight_norm(
+                nn.Conv1d(input_channels, base_channels, kernel_size=(7,), padding=3, padding_mode='replicate')),
+            # nn.GroupNorm(16, base_channels),
+            nn.ELU(inplace=True)
+        )
+
+        self.input_skip = SkipConnection(input_channels, output_channels)
+
+        self.encoder_blocks = nn.ModuleList([
+            EncoderBlock(base_channels, stride=2, n_res_units=n_res_units),
+        ])
+
+        self.middle_layers = nn.Sequential(
+            nn.utils.weight_norm(nn.Conv1d(base_channels * 2, base_channels * 2, kernel_size=(7,), padding=3,
+                                           padding_mode='replicate')),
+            # nn.GroupNorm(16, base_channels * 16),
+            nn.ELU(inplace=True),
+            nn.utils.weight_norm(nn.Conv1d(base_channels * 2, base_channels * 2, kernel_size=(7,), padding=3,
+                                           padding_mode='replicate')),
+            # nn.GroupNorm(16, base_channels * 16),
+            nn.ELU(inplace=True),
+        )
+
+        self.middle_skip = SkipConnection(base_channels * 2)
+
+        self.decoder_blocks = nn.ModuleList([
+            DecoderBlock(base_channels * 2, stride=2, n_res_units=n_res_units),
+        ])
+
+        self.decoder_skips = nn.ModuleList([
+            SkipConnection(base_channels),
+        ])
+
+        if (ismup):
+            self.out_conv = nn.Sequential(
+                nn.utils.weight_norm(
+                    mup.MuConv1d(in_channels=base_channels, out_channels=output_channels, kernel_size=7, padding=3,
+                                 padding_mode='replicate')),
+            )
+        else:
+            self.out_conv = nn.Sequential(
+                nn.utils.weight_norm(
+                    nn.Conv1d(in_channels=base_channels, out_channels=output_channels, kernel_size=7, padding=3,
+                              padding_mode='replicate')),
+            )
+
+    def forward(self, input):
+        # skip connections
+        xs = []
+        x = self.input_conv(input)
+
+        for block in self.encoder_blocks:
+            xs.append(x)
+            x = block(x)
+
+        xs.append(x)
+        x = self.middle_layers(x)
+        x = x + self.middle_skip(xs.pop())
+
+        for block, skip in zip(self.decoder_blocks, self.decoder_skips):
+            x = block(x)
+            x = x + skip(xs.pop())
+
+        x = self.out_conv(x)
+
+        return x + self.input_skip(input)
+
+
+def get_model(width=16, input_channels=1, n_res_units=3):
+    return MupAudioUNet(width, input_channels=input_channels, output_channels=1, n_res_units=n_res_units)
+
+
+if __name__ == '__main__':
+    model = get_model()
+
+    print(model)
+
+    print(sum(p.numel() for p in model.parameters() if p.requires_grad))
+
+    a = torch.zeros(1, 1, 16384)
+    with torch.no_grad():
+        y = model(a)
---
+++ mup_audio_unet_coord_check.py
@@ -0,0 +1,47 @@
+from mup import set_base_shapes, make_base_shapes
+from mup.coord_check import get_coord_data, plot_coord_data
+# construct a dictionary of lazy μP models with differing widths
+from torch.utils.data import DataLoader
+
+from data import AudioDataset
+from mup_audio_unet import VeryShallowMupAudioUNet
+
+if __name__ == '__main__':
+    # set the base shapes
+    temp_model = VeryShallowMupAudioUNet(1)
+    temp2_model = VeryShallowMupAudioUNet(2)
+    base_shapes = make_base_shapes(temp_model, temp2_model, 'audiounet_base.bsh')
+
+    def relevant_modules(name):
+        if name in ['decoder_blocks.0.layers.0.layers.0', 'decoder_blocks.1.layers.0.layers.0', 'decoder_blocks.2.layers.0.layers.0', 'decoder_blocks.3.layers.0.layers.0',
+                    'encoder_blocks.0.layers.0.layers.0', 'encoder_blocks.1.layers.0.layers.0', 'encoder_blocks.2.layers.0.layers.0', 'encoder_blocks.3.layers.0.layers.0']:
+            return True
+        return False
+
+    def lazy_model(width):
+        # `set_base_shapes` returns the model
+        return lambda: set_base_shapes(VeryShallowMupAudioUNet(width), 'audiounet_base.bsh')
+        # Note: any custom initialization with `mup.init` would need to
+        # be done inside the lambda as well
+
+
+    models = {2: lazy_model(2), 4: lazy_model(4), 8: lazy_model(8), 16: lazy_model(16), 32: lazy_model(32), 64: lazy_model(64), 128: lazy_model(128)}
+    # make a dataloader with small batch size/seq len
+    #   just for testing
+    data = AudioDataset("D:/speech_enhancement/VCTK_noised/clean_trainset_56spk_wav", aug_prob=0,
+                        test=False, segment_len=64000, dual_channel=False)
+    dataloader = DataLoader(data, batch_size=16, shuffle=False, num_workers=2)
+    # record data from the model activations over a few steps of training
+    # this returns a pandas dataframe
+    df = get_coord_data(models, dataloader, optimizer='adam', lr=3e-2, lossfn='l1')
+    # This saves the coord check plots to filename.
+    plot_coord_data(df, save_to="plot_mup.png")
+
+    def lazy_model_nomup(width):
+        return lambda: VeryShallowMupAudioUNet(width, ismup=False)
+
+    nomup_models = {2: lazy_model_nomup(2), 4: lazy_model_nomup(4), 8: lazy_model_nomup(8), 16: lazy_model_nomup(16), 32: lazy_model_nomup(32), 64: lazy_model_nomup(64), 128: lazy_model_nomup(128)}
+
+    nomup_df = get_coord_data(nomup_models, dataloader, optimizer='adam', lr=3e-2, lossfn='l1', mup=False)
+    # This saves the coord check plots to filename.
+    plot_coord_data(nomup_df, save_to="plot_nomup.png")
